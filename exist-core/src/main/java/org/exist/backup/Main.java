55d2b119d6b748774c091217e9e0d3b2c6f600a7
Adam Retter <adam.retter@googlemail.com>
2019-05-02 17:55:08 +0400

    [bugfix] Ensure that client.properties and backup.properties can be found

--- exist-core/src/main/java/org/exist/backup/Main.java
+++ exist-core/src/main/java/org/exist/backup/Main.java
@@ -41,7 +41,6 @@ import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -128,23 +127,20 @@ public class Main {
             .build();
 
     private static Properties loadProperties() {
-        // read properties
-        final Path propFile = ConfigurationHelper.lookup("backup.properties");
-        final Properties properties = new Properties();
         try {
-            if (Files.isReadable(propFile)) {
-                try(final InputStream pin = Files.newInputStream(propFile)) {
-                    properties.load(pin);
-                }
-            } else {
-                try(final InputStream pin = Main.class.getResourceAsStream("backup.properties")) {
-                    properties.load(pin);
-                }
+            final Properties properties = ConfigurationHelper.loadProperties("backup.properties", Main.class);
+            if (properties != null) {
+                return properties;
             }
+
+            System.err.println("WARN - Unable to find backup.properties");
+
         } catch (final IOException e) {
-            System.err.println("WARN - Unable to load properties from: " + propFile.toAbsolutePath().toString());
+            System.err.println("WARN - Unable to load backup.properties: " + e.getMessage());
         }
-        return properties;
+
+        // return new empty properties
+        return new Properties();
     }
 
     /**
